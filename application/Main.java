94951e793e5abbd16a30dea76e76503d9d7daef4
N-georgakopoulos <56583890+N-georgakopoulos@users.noreply.github.com>
2020-01-06 02:15:00 +0200

    Final Main.java
    Includes start() method responsible for launching the programm.

---
+++ application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import application.executiveClasses.Manager;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Manager.loadObj();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,899,500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Welcome");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
