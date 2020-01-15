60821a09786f029b4a9ca2601d17dfc1c3dfdbbd
N-georgakopoulos <56583890+N-georgakopoulos@users.noreply.github.com>
2020-01-15 02:54:00 +0200

    Add files via upload

--- application/executiveClasses/Song.java
+++ application/executiveClasses/Song.java
@@ -1,6 +1,35 @@
 package application.executiveClasses;
-
 public class Song {
-	private String name;
-	
+
+	private String songname;
+	private String albumName;
+
+	public Song(String songname, String albumName) {//create a song
+		super();
+		this.songname = songname;
+		this.albumName = albumName;
+	}
+
+//setters and getters 
+	public String getSongname() {
+		return songname;
+	}
+
+	public void setSongname(String songname) {
+		this.songname = songname;
+	}
+
+	public String getAlbumName() {
+		return albumName;
+	}
+
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+
+	@Override
+	public String toString() {
+		return "Song [songname=" + songname + ", albumName=" + albumName + "]";
+	}
+
 }
